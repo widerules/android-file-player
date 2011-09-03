c7aa15d9714e78f1535a781c274411641467de3a
umbalaconmeogia@gmail.com <umbalaconmeogia@gmail.com@f1d84171-e0c5-064f-28c8-505e211604cd>
2011-09-03 14:21:48 +0000

    Implement issue 4: Remember the last opened directory.

--- trunk/FilePlayer/src/com/chauhai/android/fileplayer/SongListActivity.java
+++ trunk/FilePlayer/src/com/chauhai/android/fileplayer/SongListActivity.java
@@ -1,14 +1,16 @@
 package com.chauhai.android.fileplayer;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
 import android.app.ListActivity;
-import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -16,13 +18,17 @@ import android.widget.TextView;
 
 public class SongListActivity extends ListActivity {
 	
-	private static final String DIR_PATH = "DIR_PATH";
+	/**
+	 * Key to save the last opened directory.
+	 */
+	private static final String PREF_CURRENT_DIRECTORY_PATH = "CURRENT_DIRECTORY_PATH";
 	
 	private static final String TAG = "SongListActivity";
+
 	/**
 	 * The root directory that the application can access.
 	 */
-	private String rootPath = Environment.getExternalStorageDirectory() + "/Music";
+	private String rootPath = Environment.getExternalStorageDirectory().toString();
 	
 	/**
 	 * The view that display the current directory.
@@ -35,19 +41,7 @@ public class SongListActivity extends ListActivity {
 	private String currentDirectoryPath = rootPath;
 	
 	private List<MusicFile> musicFiles;
-	
-    /**
-     * Call PalySongActivity to play a music file.
-     * @param context
-     * @param filePath Absolute path.
-     */
-    public static void listSongs(Context context, String dirPath) {
-    	Log.d(TAG, "listSongs " + dirPath);
-		Intent intent = new Intent(context, SongListActivity.class);
-		intent.putExtra(SongListActivity.DIR_PATH, dirPath);
-		context.startActivity(intent);
-    }
-	
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -58,16 +52,64 @@ public class SongListActivity extends ListActivity {
         currentDirectoryTextView = (TextView) findViewById(R.id.currentDirectoryTextView);
         
         // Display the song list.
-        Bundle extras = getIntent().getExtras();
-        currentDirectoryPath = extras != null ? extras.get(DIR_PATH).toString() : rootPath;
+        getDirectoryToOpen();
         displaySongList(currentDirectoryPath);
     }
+    
+    /**
+     * Save the currentDirectoryPath as the last opened directory.
+     */
+    private void setLastOpenedDirectory()
+    {
+    	Log.d(TAG, "Set last opened directory to " + currentDirectoryPath);
+    	SharedPreferences settings = getPreferences(0);
+    	SharedPreferences.Editor editor = settings.edit();
+    	editor.putString(PREF_CURRENT_DIRECTORY_PATH, currentDirectoryPath);
+    	editor.commit();
+    }
 
+    /**
+     * Get the last opened directory from the shared preferences.
+     * @return
+     */
+    private String getLastOpenedDirectory()
+    {
+//    	if (true) return rootPath + "/Music/album";
+    	String lastOpenedDir = getPreferences(0).getString(PREF_CURRENT_DIRECTORY_PATH, rootPath);
+    	Log.d(TAG, "getLastOpenedDirectory = " + lastOpenedDir);
+    	return lastOpenedDir;
+    }
+    
+    /**
+     * Get the directory to open.
+     * First, check if the parameter is passed via intent. If not, use the
+     * last opened directory or the rootPath if not specified.
+     * If the specified directory does not exist, then use its parent directory.
+     */
+    private void getDirectoryToOpen()
+    {
+    	Log.d(TAG, "getDirectoryToOpen");
+    	String currentDirectory = getLastOpenedDirectory();
+    	// If the specified directory does not exist (for example,
+    	// the lastOpenDirectory is deleted), check its parents.
+        File currentDir = new File(currentDirectory);
+        while (currentDir != null) {
+        	if (currentDir.exists() && currentDir.isDirectory()) {
+        		break;
+        	}
+        	Log.w(TAG, "Don't find directory " + currentDir.getPath() + ". Check its parent.");
+        	currentDir = currentDir.getParentFile();
+        }
+    	// If not specified, then use rootPath.
+        currentDirectoryPath = currentDir != null ? currentDir.getAbsolutePath() : rootPath;
+        Log.i(TAG, "Directory to open: " + currentDirectoryPath);
+    }
+    
     /**
      * Get all the music file in the specified directory, and display it into the list. 
      * @param dir
      */
-    private void displaySongList(String dir) {
+    protected void displaySongList(String dir) {
     	// Display the current directory path.
     	currentDirectoryPath = dir;
     	currentDirectoryTextView.setText(dir);
@@ -77,6 +119,9 @@ public class SongListActivity extends ListActivity {
     	
     	SongListAdapter adapter = new SongListAdapter(this, musicFiles);
     	setListAdapter(adapter);
+
+    	// Set the last opened directory to currentDirectoryPath.
+    	setLastOpenedDirectory();
     }
 
     /**
@@ -104,12 +149,37 @@ public class SongListActivity extends ListActivity {
     }
     
     @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK)) {
+        	// Do not go to the parent directory if reach the rootPath
+        	// (so it will be closed by the default BACK button processing).
+        	if (!currentDirectoryPath.equals(rootPath)) {
+            	// Open the parent directory.
+        		Log.d(TAG, "Goto the parent directory");
+	        	File parentDir = new File(currentDirectoryPath).getParentFile();
+	        	if (parentDir != null) {
+	        		// Open new SongListActivity
+	        		displaySongList(parentDir.getAbsolutePath());
+	                return true;
+	        	}
+        	}
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+    
+    /**
+     * Create menu when user clicks the MENU button the first time.
+     */
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.song_list, menu);
         return true;
     }
     
+    /**
+     * Handle menu when the user click MENU button.
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle item selection
